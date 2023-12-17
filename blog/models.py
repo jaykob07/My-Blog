4ce67624613cc3a4f6fc4d436f5f9c8af51b6499
jacobo <jaykob_07@hotmail.com>
2023-12-16 21:15:02 -0500

    correcion post

--- blog/models.py
+++ blog/models.py
@@ -1,8 +1,9 @@
 from django.db import models
+from django.db.models.fields import URLField
 import datetime
 
 class Post(models.Model):
     tittle = models.CharField(max_length=100)
     description = models.TextField()
     image = models.ImageField(upload_to='blog/images/')
-    date = models.DateField(datetime.date.today)
+    url = URLField(blank=True)
--- portfolio/models.py
+++ portfolio/models.py
@@ -1,9 +1,9 @@
 from django.db import models
-from django.db.models.fields import CharField, URLField
+from django.db.models.fields import CharField
 from django.db.models.fields.files import ImageField
 
 class Project(models.Model):
     title = CharField(max_length=100)
     description =CharField(max_length=250)
     image = ImageField(upload_to="iportfolio/images/")
-    url = URLField(blank=True)
+   
