0cec3ba319bcc7a2814f3d0c53babb689e50cd08
jacobo <jaykob_07@hotmail.com>
2023-12-17 19:36:21 -0500

    database fix

--- blog/models.py
+++ blog/models.py
@@ -1,9 +1,9 @@
 from django.db import models
 from django.db.models.fields import URLField
-import datetime
+
 
 class Post(models.Model):
     tittle = models.CharField(max_length=100)
     description = models.TextField()
     image = models.ImageField(upload_to='blog/images/')
-    url = URLField(blank=True)
+    url = models.URLField(blank=True)
