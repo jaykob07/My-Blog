7446701f2c86e238c5f8761a19ae47b02eae7bd2
jacobo <jaykob_07@hotmail.com>
2023-12-24 16:59:54 -0500

    edit models

--- portfolio/models.py
+++ portfolio/models.py
@@ -4,6 +4,6 @@ from django.db.models.fields.files import ImageField
 
 class Project(models.Model):
     title = CharField(max_length=100)
-    description =CharField(max_length=250)
+    description =CharField(max_length=400)
     image = ImageField(upload_to="iportfolio/images/")
    
